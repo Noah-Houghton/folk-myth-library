721e17ce22e2a2163da453f7f0b4836a2f24e0ac
Noah Houghton <9970944+Noah-Houghton@users.noreply.github.com>
2020-02-02 15:47:03 -0500

    update filter as user types

--- src/FilterField.tsx
+++ src/FilterField.tsx
@@ -2,19 +2,13 @@ import React from "react";
 import "./FilterField.css";
 
 export function FilterField(props: { handler: () => void }) {
-  function handleKeypress(e: any) {
-    const ENTER = 13;
-    if (e.keyCode === ENTER) {
-      props.handler();
-    }
-  }
   return (
     <div className="filter">
       <input
         placeholder="Enter a term to filter the library"
         id="searchTerm"
         type="text"
-        onKeyUp={handleKeypress}
+        onChange={props.handler}
       ></input>
       <button onClick={props.handler}>Filter</button>
     </div>
